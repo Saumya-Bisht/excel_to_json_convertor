519f56d49425ca1e38ebe787ab8eca91897d1869
Saumya Bisht <70054252+Saumya-Bisht@users.noreply.github.com>
2023-07-30 21:41:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,44 +5,97 @@ let change = () => {
       let jsondata = [];
 
       for (let i = 1; i < res.length; i++) {
-        let panel = {
-          "Customer ID": res[i][0],
-          "age": res[i][1],
-          "panelList": [
-            {
-              "panel_name": res[i][3],
-              "panel_code": res[i][2],
-              "parameters": [
-                {
-                  "parameterName": res[i][5],
-                  "unit": res[i][6],
-                  "parameterCode": res[i][4],
-                  "value": res[i][7],
-                  "lowerRange": res[i][8],
-                  "upperRange": res[i][9],
-                  "displayRange": res[i][10],
-                },
-              ],
-            },
-          ],
-        };
+        let customerId = res[i][0];
+        let age = res[i][1];
+        let panelCode = res[i][2];
 
-        // Check if the Customer ID already exists in jsondata
-        let existingIndex = jsondata.findIndex(
-          (item) => item["Customer ID"] === panel["Customer ID"]
+        // Find the panel with the same Customer ID and panel code
+        let existingPanelIndex = jsondata.findIndex(
+          (item) =>
+            item["Customer ID"] === customerId &&
+            item["panelList"][0]["panel_code"] === panelCode
         );
 
-        // If the Customer ID exists, append the panel to its panelList
-        if (existingIndex !== -1) {
-          jsondata[existingIndex]["panelList"].push(panel["panelList"][0]);
+        let parameter = {
+          parameterName: res[i][4],
+          unit: res[i][5],
+          parameterCode: res[i][3],
+          value: res[i][6],
+          lowerRange: res[i][7],
+          upperRange: res[i][8],
+          displayRange: res[i][9],
+        };
+
+        let panel = {
+          panel_name: res[i][2],
+          panel_code: panelCode,
+          parameters: [parameter],
+        };
+
+        // If panel with same Customer ID and panel code exists, add the parameter to it
+        if (existingPanelIndex !== -1) {
+          jsondata[existingPanelIndex]["panelList"][0]["parameters"].push(
+            parameter
+          );
         } else {
-          jsondata.push(panel);
+          // Otherwise, create a new panelList for the Customer ID
+          jsondata.push({
+            "Customer ID": customerId,
+            age: age,
+            panelList: [panel],
+          });
         }
       }
 
-      textarea.value = JSON.stringify(jsondata, null, 4);
+      textarea.value = JSON.stringify(jsondata, null, 2);
     });
   };
+// let change = () => {
+//     readXlsxFile(inp.files[0]).then((res) => {
+//       let jsondata = [];
+
+//       for (let i = 1; i < res.length; i++) {
+//         let panel = {
+//           "Customer ID": res[i][0],
+//           "age": res[i][1],
+//           "panelList": [
+//             {
+//               "panel_name": res[i][3],
+//               "panel_code": res[i][2],
+//               "parameters": [
+//                 {
+//                   "parameterName": res[i][5],
+//                   "unit": res[i][6],
+//                   "parameterCode": res[i][4],
+//                   "value": res[i][7],
+//                   "lowerRange": res[i][8],
+//                   "upperRange": res[i][9],
+//                   "displayRange": res[i][10],
+//                 },
+//               ],
+//             },
+//           ],
+//         };
+
+//         // Check if the Customer ID already exists in jsondata
+//         let existingIndex = jsondata.findIndex(
+//           (item) => item["Customer ID"] === panel["Customer ID"]
+//         );
+
+//         // If the Customer ID exists, append the panel to its panelList
+//         if (existingIndex !== -1) {
+//           jsondata[existingIndex]["panelList"].push(panel["panelList"][0]);
+//         } else {
+//           jsondata.push(panel);
+//         }
+//       }
+
+//       textarea.value = JSON.stringify(jsondata, null, 4);
+//     });
+//   };
+
+
+
 // let change=()=>{
 //     readXlsxFile(inp.files[0]).then((res)=>{
 //         console.log(res)
